a410561d58786fe3a4b74d2a6834ba300ff7774c
Hina <h.mundhwa@easternenterprise.com>
2016-05-28 15:26:02 +0530

    kapil - project end

--- src/phpreboot/tddworkshop/Calculator.php
+++ src/phpreboot/tddworkshop/Calculator.php
@@ -3,7 +3,7 @@ namespace phpreboot\tddworkshop;
 
 class Calculator
 {
-    public function add($numbers = '')
+    public function add($numbers = '', $delimeter = '')
     {
         if (empty($numbers)) {
             return 0;
@@ -12,8 +12,18 @@ class Calculator
         if (!is_string($numbers)) {
             throw new \InvalidArgumentException('Parameters must be a string');
         }
-
-        $numbersArray = explode(",", $numbers);
+        
+        if (!empty($delimeter) && ( $delimeter != '\n' || $delimeter != '\\')) {
+            throw new \InvalidArgumentException('Separators string must contain only new line characters');
+        } 
+        
+        $numbers = str_replace(array("\r\n","\r","\n","\\r","\\n","\\r\\n", "\\", ";"),",",$numbers);
+        $numbers = stripslashes($numbers);
+        $numbersArray = array_filter(explode(",", $numbers));
+        
+        if (array_filter($numbersArray, 'is_numeric') !== $numbersArray) {
+            throw new \InvalidArgumentException('Parameters string must contain numbers');
+        }
 
         return array_sum($numbersArray);
     }
--- tests/phpreboot/tddworkshop/CalculatorTest.php
+++ tests/phpreboot/tddworkshop/CalculatorTest.php
@@ -6,6 +6,39 @@ use phpreboot\tddworkshop\Calculator;
 class CalculatorTest extends \PHPUnit_Framework_TestCase
 {
     private $calculator;
+    
+    /**
+     * Data provider for invalid array provider
+     *
+     * @return array
+     */
+    public function invalidNumberProvider()
+    {
+        $numbers = "4,a,b,c";
+        return $numbers;
+    }
+    
+    /**
+     * Data provider for invalid array provider
+     *
+     * @return array
+     */
+    public function numberProvider()
+    {
+        return [["4,7,3,4,7,3,5,6,7,4,3,2,5,7,5,3,4,6,7,8,9,5,5,5,4,3,2"]];
+    }
+    
+    /**
+     * Data provider for invalid separator
+     *
+     * @return array
+     */
+    public function invalidSeparatorProvider()
+    {
+        return [
+            ['2,3,4', '%']
+        ];
+    }
 
     public function setUp()
     {
@@ -40,7 +73,7 @@ class CalculatorTest extends \PHPUnit_Framework_TestCase
     /**
       * @expectedException \InvalidArgumentException
       */
-    public function  testAddWithNonStringParameterThrowsException()
+    public function testAddWithNonStringParameterThrowsException()
     {
         $this->calculator->add(5, 'Integer parameter do not throw error');
     }
@@ -51,5 +84,36 @@ class CalculatorTest extends \PHPUnit_Framework_TestCase
     public function testAddWithNonNumbersThrowException()
     {
         $this->calculator->add('1,a', 'Invalid parameter do not throw exception');
+    }
+    
+    /**
+     * Testcase for task2
+     * @dataProvider invalidNumberProvider
+     * @expectedException \InvalidArgumentException
+     */
+    public function testAddWithStringOfNumbersReturnsTheirSumThrowException($numbers)
+    {
+        $this->calculator->add($numbers, 'Invalid parameter do not throw exception');
+    }
+    
+    /**
+     * Test case to add array of numbers
+     * Testcase for task2
+     * @dataProvider numberProvider
+     */
+    public function testAddWithStringOfNumbersReturnsTheirSum($numbers)
+    {
+        $result = $this->calculator->add($numbers);
+        $this->assertSame(133, $result, 'Invalid parameter should throw exception');
+    }   
+    
+    /**
+     * Testcase for task3
+     * @dataProvider invalidSeparatorProvider
+     * @expectedException \InvalidArgumentException
+     */
+    public function testAddWithInvalidSeparatorThrowException($numbers, $delimeter)
+    {
+        $this->calculator->add($numbers, $delimeter, 'Invalid separator throw exception');
     }
 }
